35f71625c5dd3557bd17196afc93c4532deb2596
Michael Lamb <mflambjr@gmail.com>
2021-04-11 21:54:27 -0400

    initial commit

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav>
+  <div>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </div>
+</nav>
+<slot />
+
+<style>
+  nav {
+    display: flex;
+  }
+
+  div {
+    margin: 0 auto;
+  }
+</style>
---
+++ src/routes/about.svelte
@@ -0,0 +1,14 @@
+<script>
+  const greeting = "hebbo dair!!!"
+</script>
+
+<div>
+  {greeting}
+</div>
+
+<style>
+  div {
+    background: red;
+    color: white;
+  }
+</style>
